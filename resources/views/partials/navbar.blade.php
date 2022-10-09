cb7aa4bcaeed1a5a169a3815e8d830630b6444fd
Brayan-Esten <bryanesten1221@gmail.com>
2022-10-09 21:56:12 +0700

    added navbar and footer

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid col-lg-11 d-flex justify-content-between">
+
+        <!-- left side -->
+        <div class="col-lg-5">
+            <div class="my-navbar-brand d-inline-block" data-text="PLAYVENUE">PLAYVENUE</div>
+        </div>
+
+        <!-- collapsed nav button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- right side -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav col-lg-6 d-flex justify-content-around">
+
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/">HOME</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ABOUT</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">NEWS</a>
+                </li>
+
+            </ul>
+
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        LOGIN
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
